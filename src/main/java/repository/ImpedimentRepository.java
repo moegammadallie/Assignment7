87c7d84315dbabe56f74b946c7d592ecdf525fb9
MoegammadAllie <moegammadallie89@gmail.com>
2019-04-27 17:48:59 +0200

    Assignement six

--- src/main/java/repository/ImpedimentRepository.java
+++ src/main/java/repository/ImpedimentRepository.java
@@ -1,9 +1,10 @@
 package repository;
 
 import domain.services.Impediment;
+import repository.IRepository;
 
 import java.util.Set;
 
-public interface ImpedimentRepository extends IRepository<Impediment, String>{
+public interface ImpedimentRepository extends IRepository<Impediment, String> {
         Set<Impediment> getAll();
 }
