dd7e853b69cd1f43df375aa469daffeffcdc2424
MoegammadAllie <moegammadallie89@gmail.com>
2019-04-29 00:56:34 +0200

    Assignement six

---
+++ src/test/java/repository/impl/OTToolsRepositoryImplTest.java
@@ -0,0 +1,33 @@
+package repository.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OTToolsRepositoryImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void create() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void read() {
+    }
+}
---
+++ src/test/java/repository/impl/PTToolsRepositoryImplTest.java
@@ -0,0 +1,29 @@
+package repository.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PTToolsRepositoryImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void create() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void delete() {
+    }
+}
